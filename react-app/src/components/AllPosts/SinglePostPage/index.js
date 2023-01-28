b7b6ae9c3c0ce94a4d102d20a3de393ca08cb7ef
marc <marcsmithr@gmail.com>
2023-01-27 20:22:45 -0600

    laid groundwork for singlePost page

---
+++ react-app/src/components/AllPosts/SinglePostPage/index.js
@@ -0,0 +1,67 @@
+import React, {useEffect} from 'react'
+import { useParams, Link } from 'react-router-dom'
+import './index.css'
+
+function SinglePostPage() {
+    const dispatch = useDispatch()
+    const user = post.user
+    const images = post.images
+    const { postId } = useParams()
+
+    const post= useEffect(()=> {
+        dispatch()
+    })
+
+    if(!postId) return null
+    return (
+       <div className='post-card-container'>
+
+            <div className='post-card-likes'>
+            </div>
+            <div className='post-card-main'>
+                <div className='post-info-container'>
+                    <div className='post-card-community-image-container'>
+                        <img src={post.community_image} alt={`${post.community_name} community image`} />
+                    </div>
+                    <div className='post-card-community-header-container'>
+                        <span className='post-card-community-header'>s/{post.community_name}</span>
+                    </div>
+                    <div className='post-card-user-container'>
+                        <span>· posted by u/{user.username}</span>
+                    </div>
+                </div>
+                <div className='post-title-container'>
+                    <span> {post.title} </span>
+                </div>
+                {(images[0]) &&
+                <div className='post-image-container'>
+                    <img src={images[0].url}></img>
+                </div>
+                }
+                {(post.text)&&
+                <div className='post-card-text-container'>
+                    <span>{post.text}</span>
+                </div>
+                }
+                <div className='post-interaction-container'>
+                    <div className='post-card-comment-container'>
+                        <div className='post-card-comment-icon'>
+                            <i className="fa-regular fa-comment"></i>
+                        </div>
+                        <div className='post-card-comment-text'>
+                            <span>Comments</span>
+                        </div>
+                    </div>
+                    <div className='post-card-save-container'>
+
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+    )
+}
+
+export default SinglePostPage
