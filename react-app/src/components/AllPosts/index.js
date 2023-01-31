bca1876ed77504b7954abfcc91067f37708613e2
marc <marcsmithr@gmail.com>
2023-01-31 10:41:09 -0600

    home page styled, right home div added

--- react-app/src/components/AllPosts/index.js
+++ react-app/src/components/AllPosts/index.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import PostCard from './PostCard'
 import CreatePostCommunity from '../HomePage/CreatePostCommunity'
+import CodeLangBox from '../HomePage/CodeLangBox'
+
 import './index.css'
 
 function AllPosts() {
@@ -11,19 +13,26 @@ function AllPosts() {
     const posts = Object.values(allPosts)
 
     return (
-        <div className='splash-page-body'>
-            <div className='recent-activities'>
-                <h1 id='recent-act-text'> Recent Activity </h1>
-                <div className='posts-container'>
-                    {posts.map((post) => (
-                        <PostCard
-                        post={post}
-                        key={post.id}
-                        />
-                    ))}
+        <div className='home-page-body'>
+            <div className='home-page-content'>
+                <div className='left-main-home-div'>
+                    <div className='left-main-home-header'>
+                        <h1 id='recent-act-text'> Recent Activity </h1>
+                    </div>
+                    <div className='posts-container'>
+                        {posts.map((post) => (
+                            <PostCard
+                            post={post}
+                            key={post.id}
+                            />
+                        ))}
+                    </div>
+                </div>
+                <div className='right-main-home-div'>
+                    <CreatePostCommunity/>
+                    <CodeLangBox/>
                 </div>
             </div>
-                <CreatePostCommunity/>
         </div>
     )
 }
---
+++ react-app/src/components/HomePage/CodeLangBox/index.js
@@ -0,0 +1,70 @@
+import "./index.css"
+
+
+function CodeLangBox (){
+
+    return(
+
+            <div className='code-languages-container'>
+                <div className="code-lang">
+                    <span>
+                    <i className="devicon-python-plain"></i>
+                         Python
+                    </span>
+                </div>
+                <div className="code-lang">
+                    <span>
+                    <i className="devicon-flask-original"></i>
+                        Flask
+                    </span>
+                </div>
+                <div className="code-lang">
+                    <span>
+                    <i className="devicon-git-plain"></i>
+                        Git
+                    </span>
+                </div>
+                <div className="code-lang">
+                    <span>
+                    <i className="devicon-html5-plain"></i>
+                        HTML
+                    </span>
+                </div>
+                <div className="code-lang">
+                    <span>
+                    <i className="devicon-css3-plain-wordmark"></i>
+                        CSS
+                    </span>
+                </div>
+                <div className="code-lang">
+                    <span>
+                    <i className="devicon-javascript-plain"></i>
+
+                        Javascript
+                    </span>
+                </div>
+                <div className="code-lang">
+                    <span>
+                    <i className="devicon-sqlalchemy-plain"></i>
+                        SQL Alchemy
+                    </span>
+                </div>
+                <div className="code-lang">
+                    <span>
+                    <i className="devicon-react-original"></i>
+                        React
+                    </span>
+                </div>
+                <div className="code-lang">
+                    <span>
+                    <i className="devicon-redux-original"></i>
+                        Redux
+                    </span>
+                </div>
+            </div>
+    )
+
+
+}
+
+export default CodeLangBox
